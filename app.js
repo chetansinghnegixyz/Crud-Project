38b30ab55616674ea8302a08221a01a38d009bf0
chetan <chetansinghnegi512@gmail.com>
2022-12-06 10:44:25 +0530

    Crud Project using Node JS, Express JS and MongoDB

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import connectDB from "./db/connectdb.js";
+import {join} from 'path'
+import web from "./routes/web.js";
+const app = express()
+const port = 3000
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/studentdb";
+
+connectDB(DATABASE_URL);
+
+app.use(express.urlencoded({extended:false}))
+
+app.use('/student',express.static(join(process.cwd(), "public")))
+app.use('/student/edit',express.static(join(process.cwd(), "public")))
+
+app.set("view engine", "ejs");
+
+app.use("/student", web);
+
+
+app.listen(port, () => {
+ console.log(`Server listening at http://localhost:${port}`)
+})
